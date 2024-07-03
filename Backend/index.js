e2eb006e32879c437b4ea76e5aab0e7e6a3a8fbc
sribhargav1345 <115009162+sribhargav1345@users.noreply.github.com>
2024-07-03 20:11:36 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,16 +17,8 @@ const { setupSocket } = require('./socket/socket.js');
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim());
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true 
+    origin: true, // Allows requests from all origins
+    credentials: true // Ensures credentials are included in requests
 }));
 
 app.use(cookieParser());
