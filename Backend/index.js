4491d3561a646d2c18d71f0a6939d23dd16ef813
sribhargav1345 <sribhargav1345@gmail.com>
2024-07-03 22:45:56 +0530

    Allowing All Origins

--- Backend/index.js
+++ Backend/index.js
@@ -17,15 +17,7 @@ const { setupSocket } = require('./socket/socket.js');
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim());
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: true, 
     credentials: true 
 }));
 
